aa0ac407e9c52baf0fe039c5ff16562b1b5ad933
Ayush Kelkar <ayush94klkr@gmail.com>
2026-03-22 05:39:29 +0000

    testing input payloads

---
+++ flask_main.py
@@ -0,0 +1,4 @@
+# This is the main module for everything in this server.
+# This handles all master I/O for the backend.
+# All I/O is in the form of JSONs
+# For now I'm simulating team creation locally using Postman.
---
+++ logo_launch/team_create.py
@@ -0,0 +1,6 @@
+# This is for creating a team for the logo launch event
+
+# Sample function to see if JSON Payloads work
+def create_team(payload):
+    print("Payload received.: ", payload)
+    return "Works"
